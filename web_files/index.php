d6db165c877fd7a19cb217fc0a7c00eb502c1d9f
jgottschalkmhs <49209767+jgottschalkmhs@users.noreply.github.com>
2020-11-02 11:33:02 +1300

    Initial commit

---
+++ web_files/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta name="description" content="edit me">
+	<meta name="keywords" content="edit me">
+	<meta name="author" content="you name">
+	<meta charset="UTF-8">
+	<title>Chic Clothing</title>
+	<link rel="stylesheet" href="theme/foo.css" title="style1" />
+</head>
+	
+	<body>
+<div class="content">
+
+	<div class="heading">
+		<h1 class="white">L3 DB Assessment (edit me)</h1>
+	</div> <!-- end heading -->
+
+	<div class="navigation">
+		
+		Category links go here | 
+					
+		<a class="nav" href="admin/admin.php">Admin</a>
+		
+	</div>	<!-- end navigation -->
+
+	<div class="main">
+
+		<h1>Welcome to ....</h1>
+
+		<p>Please do something or <a href="#">login</a> to access the admin panel.</p>	
+
+	</div> <!-- end main -->
+		
+	<div class="footer">
+		&copy; Your Name &copy; 20XX
+	</div> <!-- end footer -->
+
+</div> <!-- end content -->
+
+</body>
+
+
+</html>
